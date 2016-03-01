c9d40d1027ac34d5b13b34796dd7081869a19c86
Peter <Peter@128.4.120.179>
2016-03-01 02:45:15 -0500

    init

---
+++ src/main/java/base/tui.java
@@ -0,0 +1,28 @@
+package base;
+	import java.util.Scanner;
+
+public class tui {
+
+	public static void main(String[] args) {
+				Scanner input = new Scanner(System.in);
+				System.out.println("Cost of tuition?");
+				double t = input.nextInt();
+				System.out.println("Fixed APR?");
+				double apr = input.nextInt();
+				System.out.println("How many years to pay back?");
+				double years = input.nextInt();
+				double loan = 0;
+				int count = 0;
+				while (count<4){
+					loan+=t;
+					t=t*(1+(apr/100));
+					count = count+1;
+				}
+				double monthly = loan/(12*years);
+				System.out.println(monthly);
+			}
+
+
+	}
+
+
